f3dd8ab6175caded39fd49b4a5a92f1948fe5040
Petter Karstensen <petterkarstensen@gmail.com>
2024-03-15 13:44:59 +0100

    Initial project setup

---
+++ src/main/java/com/booleanuk/FinalProjectGroup4BackendApplication.java
@@ -0,0 +1,13 @@
+package com.booleanuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectGroup4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectGroup4BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booleanuk/FinalProjectGroup4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booleanuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectGroup4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
