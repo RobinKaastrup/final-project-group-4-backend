556e3ce1c6a26f4d92b7a04975bda00717d2110d
Osamah Al-maliki <osamah.al-maliki@no.experis.com>
2024-03-18 14:32:53 +0100

    Authenticating the application

--- src/main/java/com/booleanuk/model/User.java
+++ src/main/java/com/booleanuk/model/User.java
@@ -1,7 +1,6 @@
 package com.booleanuk.model;
 
 import com.booleanuk.model.Role;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
