0dc2751e74b515b81b59ef3fff7c2a89456310c6
Osamah Al-maliki <osamah.al-maliki@no.experis.com>
2024-03-21 11:27:31 +0100

    Added endpoint to UserRelationController

--- src/main/java/com/booleanuk/repository/UserRelationRepository.java
+++ src/main/java/com/booleanuk/repository/UserRelationRepository.java
@@ -1,14 +1,18 @@
 package com.booleanuk.repository;
 
+import com.booleanuk.model.User;
 import com.booleanuk.model.UserRelation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRelationRepository extends JpaRepository<UserRelation, Integer> {
 
     // Define a custom query method to find user relations by user ID
     @Query("SELECT ur FROM UserRelation ur WHERE ur.user.id = :userId")
     List<UserRelation> findByUserId(int userId);
+
+    Optional<UserRelation> findByUserAndFriend(User user, User friend);
 }
